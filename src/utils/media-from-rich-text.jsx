41c3ab81c49120ffc9e3bd0fc653dbef7e3bcdd9
xmflsct <me@xmflsct.com>
2023-10-24 22:26:58 +0200

    Update media-from-rich-text.jsx

--- src/utils/media-from-rich-text.jsx
+++ src/utils/media-from-rich-text.jsx
@@ -7,12 +7,15 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 export const mediaFromRichText = {
   renderNode: {
     [BLOCKS.EMBEDDED_ASSET]: node => {
-      if (node.data.target.fields?.file['nl-NL'].contentType.includes('image')) {
+      if (!node?.data?.target?.fields) {
+        return null
+      }
+      if (node.data.target.fields.file['nl-NL'].contentType.includes('image')) {
         return (
           <p>
             <img
               src={node.data.target.fields.file['nl-NL'].url}
-              alt={node.data.target.fields?.title['nl-NL']}
+              alt={node.data.target.fields.title['nl-NL']}
             />
           </p>
         )
@@ -20,15 +23,12 @@ export const mediaFromRichText = {
         return (
           <p>
             <a
-              href={`https:${node.data.target.fields?.file['nl-NL'].url}`}
+              href={`https:${node.data.target.fields.file['nl-NL'].url}`}
               target='_blank'
               rel='noopener noreferrer'
             >
-              {node.data.target.fields?.title['nl-NL']}{' '}
-              <FontAwesomeIcon
-                className='inline-block align-baseline h-3'
-                icon={faFileAlt}
-              />
+              {node.data.target.fields.title['nl-NL']}{' '}
+              <FontAwesomeIcon className='inline-block align-baseline h-3' icon={faFileAlt} />
             </a>
           </p>
         )
@@ -37,23 +37,13 @@ export const mediaFromRichText = {
     [INLINES.HYPERLINK]: node => (
       <a href={node.data.uri} target='_blank' rel='noopener noreferrer'>
         {node.content[0].value}{' '}
-        <FontAwesomeIcon
-          className='inline-block align-baseline h-3'
-          icon={faExternalLinkAlt}
-        />
+        <FontAwesomeIcon className='inline-block align-baseline h-3' icon={faExternalLinkAlt} />
       </a>
     ),
     [INLINES.ASSET_HYPERLINK]: node => (
-      <a
-        href={node.data.target.fields.file['nl-NL'].url}
-        target='_blank'
-        rel='noopener noreferrer'
-      >
+      <a href={node.data.target.fields.file['nl-NL'].url} target='_blank' rel='noopener noreferrer'>
         {node.content[0].value}{' '}
-        <FontAwesomeIcon
-          className='inline-block align-baseline h-3'
-          icon={faFileAlt}
-        />
+        <FontAwesomeIcon className='inline-block align-baseline h-3' icon={faFileAlt} />
       </a>
     )
   }
