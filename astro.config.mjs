fa456647f0b28e3b4fd0d46af645cf1a05e75cf1
xmflsct <me@xmflsct.com>
2026-01-19 11:21:29 +0100

    Migrate to Cloudflare Workers with npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
