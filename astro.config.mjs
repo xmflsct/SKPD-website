f510c2b450e73263892a5f1c220aa5376856567e
xmflsct <me@xmflsct.com>
2026-01-19 12:15:57 +0100

    Improve image quality

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,8 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
-    }
+    },
+    layout: 'constrained'
   },
   integrations: [sitemap()],
   vite: {
